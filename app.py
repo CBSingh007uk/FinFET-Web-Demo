6ab4e7f5bceaba1cbd525a33434abad4f956093e
Chandrabhan Singh <ckushwah@ed.ac.uk>
2025-09-08 11:20:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 
+st.title("Minimal Test App 🚀")
+
+x = np.linspace(0, 2*np.pi, 100)
+y = np.sin(x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+st.pyplot(fig)
