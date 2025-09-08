fe4c7e7e32e83613692eb241f29320fd973dd3ee
Chandrabhan Singh <ckushwah@ed.ac.uk>
2025-09-08 12:41:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-import pdfplumber
-import io
+#import pdfplumber
+#import io
 
 # ---------------------------
 # Logo
